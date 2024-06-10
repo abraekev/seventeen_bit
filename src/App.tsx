e942e031119a0fa5eeffa4ac2e81111fe6c64ee2
Braekevelt <braekevelta@delawareconsulting.com>
2024-06-10 15:09:42 +0200

    tailwind added

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import { useState } from "react";
 import Logo from "./components/Logo";
 
@@ -5,18 +6,22 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <Logo width="50%" />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-lightgrey17b space-y-4 font-sans ">
+      <Logo width="30%" />
+      <p className="bg-orange17b text-white17b font-bold py-2 px-4 rounded-full cursor-pointer hover:bg-red17b inline-block">
+        Seventeen Bit
+      </p>
 
-      <h1>Seventeen Bit</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          clicks are {count}
-        </button>
-        <p>Dutch raiding made simple </p>
-      </div>
-      <p className="read-the-docs">Copyright by some oelewappers </p>
-    </>
+      <button
+        className="bg-darkgrey17b text-white17b font-bold py-2 px-4 rounded-full cursor-pointer hover:bg-yellow17b inline-block"
+        onClick={() => setCount((currentCount) => currentCount + 1)}
+      >
+        clicks are {count}
+      </button>
+      <p className="font-bold">Dutch raiding made simple </p>
+
+      <p className="text-darkgrey17b">Copyright by some oelewappers </p>
+    </div>
   );
 }
 
