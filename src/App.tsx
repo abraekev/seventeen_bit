04eb8952664cf6c73bea82cd3d6ae7b82e8d7835
Braekevelt <braekevelta@delawareconsulting.com>
2024-06-10 13:47:31 +0200

    new logo comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import Logo from "./components/Logo";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <Logo width="50%" />
+
+      <h1>Seventeen Bit</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          clicks are {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <p>Dutch raiding made simple </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Copyright by some oelewappers </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Logo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface LogoProps {
+  width: string;
+}
+function Logo(props: LogoProps) {
+  return (
+    <img
+      src="./17bitLogoTransparent.png"
+      className="logo"
+      alt="17bit logo"
+      style={{ width: props.width }}
+    />
+  );
+}
+
+export default Logo;
